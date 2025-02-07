e2b7d6ab2a58e085f114f3713f25a860d7a6a445
Anjola Adeyemi <anjolaadeyemi4545@gmail.com>
2025-02-08 00:55:43 +0100

    Update deployment scripts and configuration for Ancient8 RPC and Etherscan

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,4 +1,3 @@
-// script/Deploy.s.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
@@ -8,49 +7,31 @@ import {Tournament} from "../src/Tournament.sol";
 import {CommunityHub} from "../src/CommunityHub.sol";
 
 contract DeployChainBets is Script {
-    function setUp() public {}
-
-    function run() public {
+    function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address opTokenAddress = vm.envAddress("OP_TOKEN_ADDRESS");
 
+        require(opTokenAddress != address(0), "OP token address not set");
+
         vm.startBroadcast(deployerPrivateKey);
 
-        // Deploy main contracts
+        // Deploy BettingPool
         BettingPool bettingPool = new BettingPool(opTokenAddress);
         console2.log("BettingPool deployed at:", address(bettingPool));
 
-        Tournament tournament = new Tournament(
-            address(bettingPool),
-            opTokenAddress
-        );
+        // Deploy Tournament
+        Tournament tournament = new Tournament(address(bettingPool), opTokenAddress);
         console2.log("Tournament deployed at:", address(tournament));
 
+        // Deploy CommunityHub
         CommunityHub communityHub = new CommunityHub(opTokenAddress);
         console2.log("CommunityHub deployed at:", address(communityHub));
 
-        // Setup contract connections
+        // Setup initial configuration
         bettingPool.setTournament(address(tournament));
         bettingPool.setCommunityHub(address(communityHub));
-
-        // Set initial protocol fee
         bettingPool.setProtocolFee(250); // 2.5%
 
         vm.stopBroadcast();
-
-        // Save deployment info
-        string memory deploymentInfo = string(
-            abi.encodePacked(
-                "Deployment timestamp: ",
-                vm.toString(block.timestamp),
-                "\nBettingPool: ",
-                vm.toString(address(bettingPool)),
-                "\nTournament: ",
-                vm.toString(address(tournament)),
-                "\nCommunityHub: ",
-                vm.toString(address(communityHub))
-            )
-        );
-        vm.writeFile("./deployments.txt", deploymentInfo);
     }
 }
